d2d04996bf85da85db8b99935dd5520486a90eab
Mikael Wikblom <mikael.wikblom@trustly.com>
2019-05-04 19:57:13 +0200

    fixed wait

--- Firebase.js
+++ Firebase.js
@@ -48,11 +48,17 @@ var _getIp = function() {
     
     return ip;
 }
-var sleep = function(ms) {
+
+function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
   
-var getIp = function() {
+  async function demo() {
+    console.log('Taking a break...');
+    await sleep(2000);
+    console.log('Two seconds later');
+  }
+async function getIp() {
     var ip = _getIp();
     if (ip) {
         return ip;
