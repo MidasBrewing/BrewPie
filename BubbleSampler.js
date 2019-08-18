28dafc3e322c034f4a4848dd35d23f5193676f47
Mikael Wikblom <mikael.wikblom@trustly.com>
2019-08-18 18:47:06 +0200

    fix

--- BubbleSampler.js
+++ BubbleSampler.js
@@ -4,7 +4,6 @@ const utils = require("./utils");
 const leastTimeBetweenBubblesInMs = utils.secs(5);
 const sendIntervalInMs = utils.mins(1);
 const frequentSendCount = 60;
-var sendNext = false;
 
 function BubbleSampler(batch) {
   this.batch = batch;
@@ -13,6 +12,7 @@ function BubbleSampler(batch) {
     console.log("Initializing bubble sampler for batch " + this.batch);
     this.currentCount = 0;
     this.sendAttemptCount = 0;
+    this.sendNext = false;
 
     this.sendInterval = setInterval(() => {
       //console.log("Maybe sending bubbles for batch " + this.batch);
