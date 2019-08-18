0fe45e9c961562a3ff68887ad1d20365fa004aaa
Mikael Wikblom <mikael.wikblom@trustly.com>
2019-08-18 13:58:04 +0200

    fixed init

--- BubbleFork.js
+++ BubbleFork.js
@@ -3,16 +3,18 @@ const BubbleSampler = require("./BubbleSampler");
 
 const BubbleFork = batch => {
   this.batch = batch;
+  this.bubbleSampler = BubbleSampler(this.batch);
 
   this.initialize = () => {
     console.log("Initializing bubble fork for batch " + this.batch);
-    this.bubbleSampler = BubbleSampler(batch);
+    this.bubbleSampler.initialize();
     this.input = new Gpio(17, "in", "rising", { debounceTimeout: 10 });
     this.input.watch(watcher);
   };
 
   this.destroy = () => {
     console.log("Destroying bubble fork for batch " + this.batch);
+    this.bubbleSampler.destroy();
     this.input.unexport();
   };
 
